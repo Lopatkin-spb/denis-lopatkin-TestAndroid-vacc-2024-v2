57649bb47be683106cd38e63ce6b2a0755d75cc8
Lopatkin-spb <lopatkin.spb@mail.ru>
2024-05-27 20:11:17 +0400

    -= test commit

---
+++ app/src/main/java/com/example/denis_lopatkin_testandroid_vacc_2024_v2/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.denis_lopatkin_testandroid_vacc_2024_v2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
